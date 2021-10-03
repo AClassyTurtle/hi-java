d64796676ff323bb999e36d23d964af8a2f41a06
myron willis <myronwillis@outlook.com>
2021-10-03 16:31:46 +0000

    hi java

---
+++ Hi.java
@@ -0,0 +1,11 @@
+public class SayHiinJava{
+	public static void main(String[] args){
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+
+	}
+}
+
